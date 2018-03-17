4d63a60210a98253034bbd3d68fa2d9df722a757
Rami Alayan <wink_dev@outlook.com>
2018-03-17 16:40:11 +0200

    Capsulating, fixed animation, start of the explosion animation

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,61 +2,98 @@ $(document).ready(function(){
   var docWidth, docHeight, mainBtn_width, mainBtn_height;
   var currentPosition;
   var hoverInterval;
-  var height = 100;
+
+  var circle, circleSize, circlePosition;
+  //document height and width
   docWidth = $("body").width();
   docHeight = $("body").height();
 
+  //gets the height and width of the main starting button
   mainBtn_width = $(".main-btn").width();
   mainBtn_height = $(".main-btn").height();
 
+  //gets the center of the document
   var centerPoint = {
     "top" : docHeight/2,
     "left" : docWidth/2
   };
 
-  currentPosition = $(".main-btn").position();
-  function mainBtnHover() {
-
-    $(".main-btn").animate(
-      {top: currentPosition.top + 10 + "px"},1500
-    ). animate(
-      {top: currentPosition.top - 20 + "px"},1500
-    );
-  }
-
-
-  mainBtnHover();
-  hoverInterval = setInterval(function(){mainBtnHover();}, 3000);
+//FUNCTIONS FOR EVENT LISTENRES {
 
   //on click explosion animation that opens all the buttons
   var explosion = function(){
+    stopHoverAnimation();
+    clearInterval(hoverInterval);
+    $(".main-btn").stop(true, false);
+    //generates all the circles and sets the positions
     for (i = 0; i < 7; i++){
-      var circle = "<div id='c"+ i + "' class='circles'></div>";
 
-
+      circle = "<div id='c"+ i + "' class='circles'></div>";
       $("#main-container").append(circle);
-      $("#c"+i).css({
-        "top": "100px"
-      });
-    }
+
+      circleSize = Math.floor( Math.random() * 100) + 50 + "px";
+      console.log(circleSize);
+
+      currentPosition = $("#c"+i).position();
+      circlePosition = {
+        "top": centerPoint.top - Math.floor( Math.random() * 100) - 100 + "px",
+        "left": centerPoint.left - Math.floor( Math.random() * 100) + 200 + "px"
+      }
+
+      $("#c"+i).animate({
+        'width': circleSize,
+        'height': circleSize,
+        'left': circlePosition.left,
+        'top': circlePosition.top
+
+      }); //end of #c animate
+    } //end of for loop
+
+    $(this).unbind("click",explosion);
+    $(this).unbind("mouseenter",stopHoverAnimation);
+    $(this).unbind("mouseleave",resetHoverAnmiation);
+  } // end of explosion funciton
+
+  function mainBtnHover() {
+    currentPosition = $(".main-btn").position();
+    $(".main-btn").animate({
+      top: centerPoint.top - mainBtn_height/2 - 20 + "px"
+    },2000,function(){
+      $(this).animate({
+        top: centerPoint.top - mainBtn_height/2 + 20 + "px"
+      },2000);
+    });
   }
 
+  function initBtnHover(){
+    mainBtnHover();
+    hoverInterval = setInterval(function(){mainBtnHover();}, 4000);
+  }
+
+  function stopHoverAnimation(){
+    $(".main-btn").stop(true,false).animate({
+      top: centerPoint.top - mainBtn_height/2
+    },1000);
+    clearInterval(hoverInterval);
+  }
+
+  function resetHoverAnmiation(){
+    initBtnHover();
+  }
+
+  //END OF FUNCTIONS FOR EVENT LISTENRES }
+
+  //INITIATING ANIMATIONS {
+  initBtnHover();
+
+  //EVENT LISTENERES
+  $(".main-btn").bind("mouseenter", stopHoverAnimation);
+  $(".main-btn").bind("mouseleave", resetHoverAnmiation);
+
+  //opens all the buttons on click on the main button
   $(".main-btn").bind("click", explosion);
 
-  $(".main-btn").mouseenter(function(){
-    clearInterval(hoverInterval);
-    $(this).stop(true,false).animate({
-      top: centerPoint.top - mainBtn_height/2,
-      left: centerPoint.left - mainBtn_width/2
-    },1000);
-  });
 
-
-
-  $(".main-btn").mouseleave(function(){
-
-    mainBtnHover();
-    hoverInterval = setInterval(function(){mainBtnHover();}, 3000);
-  });
+  //END OF EVENT LISTENERES }
 
 });
